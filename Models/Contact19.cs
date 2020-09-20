ee32c67d20498b13ba3654d716dceca4b4956793
Kübra Yılmaz <70054802+kubrayilmaz06@users.noreply.github.com>
2020-09-21 00:51:52 +0300

    Add files via upload

---
+++ Models/Contact19.cs
@@ -0,0 +1,25 @@
+﻿namespace cogenNugetContact.Models.General
+{
+    public class Contact19
+    {
+        public string cCompanyName { get; set; }
+        public string cPhone { get; set; }
+        public string cGsm { get; set; }
+        public string cFax { get; set; }
+        public string cAdress { get; set; }
+        public string cEmailAdress { get; set; }
+        public string cFacebookURL { get; set; }
+        public string cTwitterURL { get; set; }
+        public string cGooglePlusURL { get; set; }
+        public string cYoutubeURL { get; set; }
+        public string cInstagramURL { get; set; }
+        public string cLinkedinURL { get; set; }
+        public string cFoursquareURL { get; set; }
+        public string cLatitude { get; set; }
+        public string cLongitude { get; set; }
+        public string cPinterestURL { get; set; }
+        public string cGoogleMapsLink { get; set; }
+        public string cWhatsApp { get; set; }
+        public string cSkype { get; set; }
+    }
+}
