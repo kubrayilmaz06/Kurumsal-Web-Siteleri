2a324622cbdb41ccb653db40ed9c61cf272320c6
Kübra Yılmaz <70054802+kubrayilmaz06@users.noreply.github.com>
2020-09-20 22:06:07 +0300

    Add files via upload

---
+++ Models/Service6.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace cogenNuGetService2.Models.HomePage
+{
+    public class Service6
+    {
+        public string cSubTitle { get; set; }
+        public string cTitle { get; set; }
+        public string cDescription { get; set; }
+        public string cUrl { get; set; }
+        public List<ListModel> listModel { get; set; }
+
+        public class ListModel
+        {
+            public string cImage { get; set; }
+            public string cTitle { get; set; }
+            public string cDescription { get; set; }
+            public string cUrl { get; set; }
+        }
+    }
+}
