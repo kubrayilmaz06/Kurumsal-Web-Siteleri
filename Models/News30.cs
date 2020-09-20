95628f5e0e4eaeb0946b0f4560f48db623adf2f5
Kübra Yılmaz <70054802+kubrayilmaz06@users.noreply.github.com>
2020-09-20 22:39:39 +0300

    Add files via upload

---
+++ Models/News30.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cogenNuGetNews.Models.HomePage
+{
+    public class News30
+    {
+        public string cTitle { get; set; }
+        public List<ListModel> listModel { get; set; }
+
+        public class ListModel
+        {
+            public string cTitle { get; set; }
+            public string cUrl { get; set; }
+            public string cImage { get; set; }
+            public string cDescription { get; set; }
+            public int iOkunma { get; set; }
+            public string cKategori { get; set; }
+            public DateTime dNewDate { get; set; }
+
+        }
+
+    }
+}
