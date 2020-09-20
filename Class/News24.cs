78aac5727decfc9ef20d0da62d756718dc62e78f
Kübra Yılmaz <70054802+kubrayilmaz06@users.noreply.github.com>
2020-09-20 22:41:54 +0300

    Add files via upload

---
+++ Class/News24.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cogenNuGetNews.Models.HomePage
+{
+    public class News24
+    {
+        public string cTitle { get; set; }
+        public string cDescription { get; set; }
+        public string cUrl { get; set; }
+        public List<ListModel> listModel { get; set; }
+
+        public class ListModel
+        {
+            public string cImage { get; set; }
+            public DateTime dNewDate { get; set; }
+            public string cTitle { get; set; }
+            public string cDescription { get; set; }
+            public string cUrl { get; set; }
+            public int iOkunma { get; set; }
+
+        }
+    }
+}
