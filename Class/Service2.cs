aacb59dd613d843b94fe25490618da63d5b82a64
Kübra Yılmaz <70054802+kubrayilmaz06@users.noreply.github.com>
2020-09-20 21:59:43 +0300

    Update Service2.cs

--- Class/Service2.cs
+++ Class/Service2.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace cogenNuGetService2.Class.HomePage
+namespace Service2.Class.HomePage
 {
     public class Service2
     {
