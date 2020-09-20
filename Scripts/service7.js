46c2dd4c3cd686f8c20461f2cc6c0535b5ae50ab
Kübra Yılmaz <70054802+kubrayilmaz06@users.noreply.github.com>
2020-09-20 22:11:16 +0300

    Add files via upload

---
+++ Scripts/service7.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    $("#owl-carousel-service-7").owlCarousel({
+        margin:0,
+        items: 1,
+        loop: true,
+        center: false,
+        mouseDrag: true,
+        nav: true,
+        dots: false,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            720: {
+                items: 2
+            },
+            960: {
+                items: 2
+            },
+            1140: {
+                items: 3
+            }
+        },
+        navText: ["<i class=\"fa fa-angle-left\" aria-hidden=\"true\"></i>", "<i class=\"fa fa-angle-right\" aria-hidden=\"true\"></i>"]
+    });
+});
