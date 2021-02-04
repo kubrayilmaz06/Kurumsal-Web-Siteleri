a1a8759ecc5a1fe00a97b3304225c30d862b2965
Kübra Yılmaz <70054802+kubrayilmaz06@users.noreply.github.com>
2021-02-04 09:29:57 +0300

    Update and rename Service11.cs to sampleService4.cs

--- Models/sampleService4.cs
+++ Models/sampleService4.cs
@@ -1,8 +1,8 @@
 ﻿using System.Collections.Generic;
 
-namespace Service2.Models.HomePage
+namespace Service.Models.HomePage
 {
-    public class Service11
+    public class Service
     {
         public string cTitle { get; set; }
         public string cSubTitle { get; set; }
